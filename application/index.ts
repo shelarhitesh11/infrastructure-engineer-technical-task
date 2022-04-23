8b3f30c228fa9398a248a8638913b0aa1e987e3f
shelarhitesh11 <shelarhitesh11@gmail.com>
2022-04-23 03:38:19 +0000

    This is done on 23-apr-2022

---
+++ application/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import winston from 'winston';
+import expressWinston from 'express-winston';
+
+const app = express();
+const PORT = 3000;
+
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console()
+    ],
+    meta: false,
+    msg: "HTTP  ",
+    expressFormat: true,
+    colorize: false,
+    ignoreRoute: function (req, res) { return false; }
+}));
+
+app.get('/', (req, res) => {
+    res.send('You have successfully run the import.io application!');
+});
+
+app.get('/import/:userId', function (req, res) {
+    res.send(req.params);
+});
+
+app.all('*', function (req, res, next) {
+    res.send(req.params);
+});
+
+app.listen(PORT, () => {
+    console.log(`Express server is listening at ${PORT}`);
+});
